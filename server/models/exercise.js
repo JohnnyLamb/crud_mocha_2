d8de23ec085cfb14801cbce19d138d47d28d7ef9
johnny lamb <agnusjack@gmail.com>
2015-09-22 17:36:12 -0600

    first commit

---
+++ server/models/exercise.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Exercise = new Schema({
+  name: String,
+  difficulty: Number
+});
+
+module.exports = mongoose.model('exercise', Exercise);
+
