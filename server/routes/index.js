de27793a1073aad5c032c0a1ae02786c4d845975
johnny lamb <agnusjack@gmail.com>
2015-09-22 19:04:51 -0600

    passed all tests

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,73 @@
 var express = require('express');
 var router = express.Router();
+var Exercise = require('../models/exercise');
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/exercises',function(req,res,next){
+  Exercise.find(function(err,data){
+    if(err){
+      res.json({'message':err});
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+router.get('/exercises/:id',function(req,res,next){
+  Exercise.find(function(err,data){
+    if(err){
+      res.json({'message':err});
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+router.post('/exercises/:name/:difficulty',function(req,res,next){
+  var newExercise = new Exercise({
+    name: req.params.name,
+    difficulty: req.params.difficulty
+  });
+  newExercise.save(function(err,data){
+    if(err){
+      res.json({'message':err});
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+router.put('/exercise/:id/:name/:difficulty',function(req,res,next){
+   var query={
+    _id: req.params.id
+  };
+  var update = new Exercise({
+    name: req.params.name,
+    difficulty: req.params.difficulty
+  });
+
+  Exercise.findOneAndUpdate(query,update,function(err,data){
+    if(err){
+      res.json({'message':err});
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+router.delete('/exercises/:id',function(req,res,next){
+  Exercise.remove(function(err,data){
+    if(err){
+      res.json({'message':err});
+    } else {
+      res.json(data);
+    }
+  });
+});
+
 
 module.exports = router;
--- test/test-exercises.js
+++ test/test-exercises.js
@@ -8,3 +8,97 @@ var Exercise = require("../server/models/exercise");
 
 var should = chai.should();
 chai.use(chaiHttp);
+
+describe('Exercise',function(){
+  Exercise.collection.drop();
+  var id;
+  beforeEach(function(done){
+    var newExercise = new Exercise({
+      name:'pushups',
+      difficulty:3
+    });
+    id = newExercise._id;
+    newExercise.save(function(err){
+      done();
+    });
+  });
+  afterEach(function(done){
+    Exercise.collection.drop();
+    done();
+  });
+
+  it('should list All exercises on /exercises GET',function(done){
+    chai.request(server)
+      .get('/exercises')
+      .end(function(err,res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('name');
+        res.body[0].should.have.property('difficulty');
+        res.body[0].name.should.equal('pushups');
+        res.body[0].difficulty.should.equal(3);
+        done();
+      });
+
+  });
+
+  it('should create one exercise on /exercises POST',function(done){
+    chai.request(server)
+      .post('/exercises/pushups/3')
+      .end(function(err,res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.should.be.a('object');
+        res.body.name.should.be.a('string');
+        done();
+      });
+  });
+
+ it('should list SINGLE exercise on /exercise/:id GET', function(done) {
+    var newExercise = new Exercise({
+      name: 'pushups',
+      difficulty:3
+    });
+    newExercise.save(function(err, data) {
+      chai.request(server)
+        .get('/exercises/' + id)
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          // res.body.should.be.a('array');
+          res.body[0].should.be.a('object');
+          res.body[0].name.should.be.a('string');
+          res.body[0].name.should.be.equal('pushups');
+
+          res.body[0].difficulty.should.be.equal(3);
+          done();
+        });
+    });
+  });
+
+
+it('should update a SINGLE exercise on /exercise/<id> PUT', function(done) {
+
+      chai.request(server)
+        .put('/exercise/'+id+ '/curl/2')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+      });
+    });
+
+it('should delete a SINGLE exercise on /exercise/<id> DELETE', function(done) {
+    chai.request(server)
+      .delete('/exercises/' + id)
+      .end(function(error, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        done();
+      });
+  });
+
+});
